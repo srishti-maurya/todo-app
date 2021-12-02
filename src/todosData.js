fece4b3d3ff543b140f52c6665564aefe22a240a
srishti-maurya <srishti22maurya@gmail.com>
2021-12-02 12:29:36 +0530

    basic setup

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Take out the trash",
+    completed: true,
+  },
+  {
+    id: 2,
+    text: "Grocery Shopping",
+    completed: false,
+  },
+  {
+    id: 3,
+    text: "clean tank",
+    completed: false,
+  },
+  {
+    id: 4,
+    text: "Mow lawn",
+    completed: true,
+  },
+  {
+    id: 5,
+    text: "Catch up on arrested Development",
+    completed: false,
+  },
+];
+
+export default todosData;
